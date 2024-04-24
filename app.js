41fb4212daebe0ceea252f2e5cdb5bf15fc30379
Naka <110599493+Naka19spc@users.noreply.github.com>
2024-04-24 05:20:45 -0700

    add changes

---
+++ app.js
@@ -0,0 +1,126 @@
+// First Lesson
+// let num =100;
+
+// if(num <50){
+//     console.log("less than  50")
+
+// }else if(num >20){
+//     console.log("More than 20")
+
+// }else{
+//     console.log("Error")
+// }
+
+
+// let fName ="Mariam"
+// if(fName === "Mariam"){
+//     console.log(true)
+// }else{
+//     console.log(fakse)
+// }
+
+
+// switch (fName) {
+//     case "Mariam":
+//         console.log(true);
+//         break
+//     default:
+//         console.log(false)
+// }
+
+// Second Lesson
+
+// for(let i =5; i<100; i++){
+  
+//     console.log(i)
+// }
+
+// let array1= [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+// for(let i=0; i<array1.length; i++){
+//     let sm=array1[i]
+//     if(sm>0&&sm<10)
+//     console.log(sm)
+// }
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for(let item of array2){
+//     if(item === 5)
+//     break;
+//     console.log("it is")
+// }
+
+// let array3= [1, 2, 3, 4, 5];
+// let sum =0;
+// for(let item of array3){
+// sum = (sum + item)/array3.length}
+// console.log(sum, "Test");
+
+// let array5 = [1, 2, 3, 7, 6, 9];
+
+// for(let item of array5){
+//     if(item===7){
+//         continue;
+//     }
+//     console.log(item);
+// }
+
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25, studentstatus: "active"
+// };
+// console.log(user.studentstatus);
+
+// let user = {
+//     name: 'giorgi',
+//     age: 20,
+//     studentstatus: 'active'
+//     }
+
+//     if(user.age<18&& user.studentstatus ==="active"){
+//         console.log("Hello");
+//     }else if(user.name ==="Levan"){
+//         console.log("Hello Levan");
+//     }else if(user.studentstatus === "active" || user.age < 25){
+//         console.log("Hello World");
+
+//     }else{
+//         console.log("Error");
+//     }
+
+    // let array = [
+    //     [2, -3, 5, 10],
+    //     [25, -24, -11, 100],
+    //     [-6, -7, 10],
+    //     ];
+
+    //    for(let item of array){
+    //     for(let i of item){
+    //        if(i <0){
+    //         continue
+    //        }
+    //        console.log(i);
+    //     }
+    //    }
+
+    // let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ] 
+    // for(let i =0; i<array.length; i++){
+    //     let num =array[i];
+    //     if(num%2 ===1){
+    //         console.log(num);
+    //     }
+    // }
+
+    // let users = [
+    //     {username: 'giorgi', status: false},
+    //     {username: 'levani', status: false},
+    //     {username: 'anna', status: true}
+    //     ]
+
+    //     for(let item of users){
+    //         if(item.status === true){
+    //             console.log(item);
+    //         }
+    //     }
+
+
+    // Third Lesson
